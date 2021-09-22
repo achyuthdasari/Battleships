d7943d66e5d74b48b6fd77bee3545beb351b54aa
achyuthdasari <achyuthdasari.24@gmail.com>
2021-09-22 12:28:55 +0530

    completed the implementation of emptyGrid

--- battleship.py
+++ battleship.py
@@ -62,7 +62,9 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    return
+    # code
+    twoDimList=[[1 for j in range(cols)] for i in range(rows)]
+    return twoDimList
 
 
 '''
@@ -268,6 +270,8 @@ def runSimulation(w, h):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
+    test.testEmptyGrid()
 
     ## Finally, run the simulation to test it manually ##
     # runSimulation(500, 500)
+
